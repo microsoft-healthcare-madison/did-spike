44d761a45a1b8d2238efcc4ef8b444e9d3d623a6
Josh Mandel <jmandel@med.harvard.edu>
2019-10-16 16:59:25 -0500

    Add conversion from Jws back to Vc

--- issuer/vcToJws.js
+++ issuer/vcToJws.js
@@ -2,6 +2,8 @@ import { createIdentity } from 'nacl-did';
 
 var fs = require('fs');
 
+const deepCopy = a => JSON.parse(JSON.stringify(a))
+
 function convertVcToJws(vc) {
   // **** start with basic fields ****
 
@@ -9,7 +11,7 @@ function convertVcToJws(vc) {
     iss: vc.issuer,
     jti: vc.id,
     sub: vc.credentialSubject.id,
-    vc: vc,
+    vc: deepCopy(vc),
   };
 
   // **** convert UTC dates into Unix Timestamps ****
@@ -45,6 +47,23 @@ function convertVcToJws(vc) {
   return jws;
 }
 
+const unixToIso = s => new Date(s * 1000).toISOString()
+
+function convertJwsToVc(jws) {
+  return {
+    ...deepCopy(jws.vc),
+    issuer: jws.iss,
+    id: jws.jti,
+    credentialSubject: {
+      ...deepCopy(jws.vc.credentialSubject),
+      id: jws.sub
+    },
+    expirationDate: unixToIso(jws.exp),
+    issuanceDate: unixToIso(jws.nbf),
+  }
+}
+
+
 function signJws(jws, identity) {
   jws.iss = identity.did;
   return identity.createJWT(jws);
@@ -64,6 +83,12 @@ let jws = convertVcToJws(vc);
 // **** log ****
 
 console.log('Converted to JWS:');
+console.log(JSON.stringify(jws, null, 2));
+
+let backToVc = convertJwsToVc(jws);
+console.log('Converted back to VC:');
+console.log(JSON.stringify(backToVc, null, 2));
+
 console.log(JSON.stringify(jws, null, 2));
 
 // **** sign the JWS, just create an identity for testing ****
