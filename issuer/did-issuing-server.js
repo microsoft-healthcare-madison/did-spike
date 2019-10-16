e146eac78d9889e82395ecee83d0d630fcd9118d
Carl Anderson <carl.anderson@microsoft.com>
2019-10-16 16:49:22 -0500

    wip: current status of issuing server

--- issuer/did-issuing-server.js
+++ issuer/did-issuing-server.js
@@ -1,20 +1,66 @@
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
-var cors = require('cors');
+const uuid = require('uuid');
 const app = express();
 
-// **** configure cors - default configuration is any-origin ****
-
-app.use(cors());
-
 // **** use PORT env variable or 3000 ****
-
 const port = process.env.PORT || 3000;
 
 import { createIdentity } from 'nacl-did';
 
-// TODO: load this from a local file, if requested
 const identity = createIdentity();
+
+const verifications = new Map();
+
+class VerificationRequest {
+  constructor(request) {
+    this.creationTs = Date.now();
+    this.id = uuid.v4();
+    this.request = request;
+    verifications.set(this.id, this);
+    this.challenge = null;
+  }
+
+  establish() {
+    if (!this.verifyFhirServerAccess()) {
+      // TODO: failed to access server - we won't be sending any SMS messages
+      return false;
+    }
+    this.challenge = this.issueChallenge();
+  }
+
+  verifyFhirServerAccess() {
+    // TODO: confirm FHIR server access
+    return true;  // XXX
+  }
+
+  issueChallenge() {
+    // TODO: create (and send) a request through Twilio
+    // TODO: raise an exception if Twilio is down or the challenge fails to send
+    return null;  // XXX
+  }
+
+  processChallengeResponse(res) {
+    // TODO: pass a challenge response back through Twilio
+    return true;  // XXX
+  }
+}
+
+const confirm = (req, res) => {
+  const plain = identity.decrypt(req.body);
+  // TODO: raise an exception if the body fails decryption.
+  const verificationID = plain.verificationID;
+  if (!verifications.has(verificationID)) {
+    res.send('LOL WUT - I dont know your verification id: ' + verificationID);
+    return;  // XXX - raise an excecption?
+  }
+  const verification = verifications.get(verificationID);
+  if (!verification.processChallengeResponse(req.body)) {
+    res.send('That code failed to pass the challenge response');
+    return;  // XXX - raise an exception?
+  }
+};
 
 // TODO: determine if app.all can be used to insert response headers without
 //       requiring any changes at the other handlers.
@@ -30,18 +76,25 @@ const did = (req, res) => {
   decorate(res).send(identity.did);
 }
 
-// establish an SMS claim
 const verify = (req, res) => {
-  console.log(JSON.stringify(req.body, null, 2));
-  // TODO: what are the required components in the request?
-  decorate(res).send('SMS<p><pre>' + JSON.stringify(identity, null, 2) + '</pre>');
+  console.log(JSON.stringify(req.body, null, 2));  // XXX
+  // TODO: decrypt the request using the identity.
+  const plain = identity.decrypt(req.body);
+  console.log(JSON.stringify(plain, null, 2));  // XXX
+  const request = new VerificationRequest(plain);
+  // TODO: should the response be encrypted using the UI's DID?
+  if (request.establish()) {
+    decorate(res).send(request.id);
+  }
 }
 
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 
 app.get('/', (req, res) => res.json(identity));
+app.post('/confirm', confirm);
 app.get('/did', did);
 app.get('/debug', debug);
 app.post('/verify', verify);
