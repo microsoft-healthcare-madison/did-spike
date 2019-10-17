1d86a3d36e7dc12b0a9adf8404bf2575a543f823
Josh Mandel <jmandel@med.harvard.edu>
2019-10-17 18:28:34 -0500

    Use storeon for state

--- issuer/did-issuing-server.js
+++ issuer/did-issuing-server.js
@@ -1,12 +1,14 @@
-import { createIdentity } from 'nacl-did';
-import { TranscriptionContext } from 'twilio/lib/rest/api/v2010/account/transcription';
-
 //const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
+import  fetch, {Headers} from 'node-fetch'
 const cors = require('cors');
 const express = require('express');
 const uuid = require('uuid');
 const events = require('events');
+
+import createStore from 'storeon'
+import { createIdentity } from 'nacl-did';
+import { TranscriptionContext } from 'twilio/lib/rest/api/v2010/account/transcription';
+
 
 const app = express();
 
@@ -31,10 +33,6 @@ const _verificationStates = {
   ERROR: 'ERROR',
 }
 
-const _stateEvents = {
-  
-}
-
 const _errors = {
   INVALID_ARGUMENT: 
     {errorCode: -1, errorType: 'Invalid arguments'},
@@ -52,7 +50,6 @@ const _errors = {
 // **** use PORT env variable or 3000 ****
 const port = process.env.PORT || 3000;
 
-
 const twilioConfig = {
   accountSid: process.env.TWILIO_ACCOUNT_SID,
   authToken: process.env.TWILIO_AUTH_TOKEN,
@@ -66,226 +63,15 @@ const identity = createIdentity();
 const verifications = new Map();
 
 const fhirPatientHasContactPoint = (patient, contactPoint) =>
-  (patient.telecom || []).filter(t => (
-    t.system === contactPoint.system &&
-    t.value === value.use &&
+  (patient.telecom || []).filter(t =>  {
+    return (t.system === contactPoint.system &&
+    t.value === contactPoint.value &&
     t.use === contactPoint.use &&
     t.use !== 'old'
-  )).length > 0
-
-const genReturn = (success, error, details) => {
-  if (success) {
-    return {type: 'success'};
+  )
   }
-  return {type: 'error', error: {...error, errorDetails: details}};
-}
+).length > 0
 
-const _success = genReturn(true, null, null);
-
-class VerificationRequest {
-  constructor(request) {
-    this.creationTs = Date.now();
-    this.id = uuid.v4();
-    this.request = request;
-    verifications.set(this.id, this);
-    this.challenge = null;
-    this.retrievedFhirResourceBody = null;
-    this.state = _verificationStates.INIT;
-    this.error = {};
-    this.establish().then(result => this.processEvent(result))
-  }
-
-  async processEvent(incomingEvent){
-    const [delta, nextFn] = this.transitionsForEvent( {state: this.state, error: this.error }, incomingEvent)
-    
-    if (delta.state) {this.state = delta.state}
-    if (delta.error) {this.error = delta.error}
-    if (nextFn) {
-      const nextEvent = nextFn();
-      this.processEvent(nextEvent)
-    }
-
-  }
-
-
-  async transitionsForEvent(current, incomingEvent) {
-
-    const terminalStates = [_verificationStates.ERROR, _verificationStates.CONTACT_VERIFIED]
-
-    const handlerForState = {
-      [_verificationStates.INIT]: {
-        transitions: {
-          'success': [{state: _verificationStates.ESTABLISHED}, () => this.tryGetFhirResource()],
-          'error': [{state: _verificationStates.ERROR, error: incomingEvent.error}]
-        }
-      },
-      [_verificationStates.ESTABLISHED]: {
-        transitions: {
-          'success': [{state: _verificationStates.FHIR_RETRIEVED}, () => this.tryValidateResourceClaim](),
-          'error': [{state: _verificationStates.ERROR, error: incomingEvent.error}]
-        }
-      },
-      [_verificationStates.FHIR_RETRIEVED]: {
-        transitions: {
-          'success': [{state: _verificationStates.FHIR_VERIFIED}, () => this.issueChallenge()],
-          'error': [{state: _verificationStates.ERROR, error: incomingEvent.error}]
-        }
-      },
-      [_verificationStates.FHIR_VERIFIED]: {
-        transitions: {
-          'success': [{state: _verificationStates.CONTACT_VERIFYING}],
-          'error': [{state: _verificationStates.ERROR, error: incomingEvent.error}]
-        }
-      },
-      [_verificationStates.CONTACT_VERIFYING]: {
-        transitions: {
-          'success': [{state: _verificationStates.CONTACT_VERIFIED}],
-          'error': [{state: _verificationStates.ERROR, error: incomingEvent.error}]
-        }
-      }
-    }
-
-    const handler = handlerForState[current.state]
-    return handler.transitions[incomingEvent.type]
-  }
-  
-
-  /**
-   * Basic check to ensure the request is valid
-   */
-  async establish() {
-    // **** sanity checks ****
-
-    if (!this.request) {
-      return genReturn(false, _errors.INVALID_ARGUMENT, 'Request not found')
-    }
-
-    if (!this.request.fhirBaseUrl) {
-      return genReturn(false, _errors.INVALID_ARGUMENT, 'No FHIR server URL');
-    }
-
-    if (!this.request.resourceType) {
-      return genReturn(false, _errors.INVALID_ARGUMENT, 'No FHIR Resource Type');
-    }
-
-    if (!this.request.resourceId) {
-      return genReturn(false, _errors.INVALID_ARGUMENT, 'No FHIR Resource ID');
-    }
-
-    // **** success ****
-
-    return (_success);
-  }
-  
-
-  /**
-   * Check to see if we can access the FHIR server and pull the specified resource
-   */
-  async tryGetFhirResource() {
-    // **** build the URL for our request ****
-
-    let url = new URL(
-      `${this.request.resourceType}/${this.request.resourceId}`,
-      this.request.fhirBaseUrl
-    ).toString();
-
-    // **** build the headers for our FHIR request ****
-
-    let headers = new Headers();
-    headers.append('Accept', 'application/fhir+json');
-    headers.append('Authorization', this.request.authToken);
-
-    // **** issue our request ****
-
-    let response = await fetch(url, {
-      method: 'GET',
-      headers: headers,
-    });
-
-    // **** check for status ****
-
-    if (response.ok) {
-      // **** grab the body of the response ****
-
-      this.retrievedFhirResourceBody = await response.text();
-
-      if (!this.retrievedFhirResourceBody) {
-        return genReturn(false, _errors.RESOURCE_NOT_FOUND, `Request to ${url} returned no content.`);
-      }
-
-      // **** success ****
-
-      return (_success);
-    }
-
-    // **** flag our error ****
-    return genReturn(
-      false, 
-      _errors.FAILED_TO_CONNECT_TO_FHIR_SERVER, 
-      `Request to ${url} failed: `+
-        `${response.statusCode} - ${response.statusMessage}`
-      );
-  }
-
-  /**
-   * Check a retrieved resource against the verification info
-   */
-  tryValidateResourceClaim() {
-    // **** act depending on resource type ****
-
-    switch (this.request.resourceType) {
-      case 'Patient':
-        if (fhirPatientHasContactPoint(
-          this.retrievedFhirResourceBody,
-          this.request.contactPoint
-        )) {
-          return (_success);
-        }
-
-        return genReturn(
-          false, 
-          _errors.CONTACT_POINT_NOT_FOUND, 
-          `Contact point not found! ${JSON.stringify(this.request.contactPoint)}`
-          );
-        // break;
-    }
-
-    // **** unknown resource type, cannot verify ****
-
-    return genReturn(
-      false, 
-      _errors.INVALID_ARGUMENT, 
-      `Unsupported resource type: ${this.request.resourceType}`
-      );
-  }
-
-  async issueChallenge() {
-    // TODO: raise an exception if Twilio is down or the challenge fails to send
-    const twilioResponse = await twilioClient
-      .verify.services(twilioConfig.serviceId)
-      .verifications
-      .create({
-        to: this.request.contactPoint.value,
-        channel: this.request.verifyMethod
-      })
-    
-    console.log("Twilio issued", twilioResponse)
-    return genReturn(true)
-  }
-
-  async processChallengeResponse() {
-    // TODO: pass a challenge response back through Twilio
-    const twilioResponse = await twilioClient.verify.services(twilioConfig.serviceId)
-      .verificationChecks
-      .create({
-        to: this.request.contactPoint.value,
-        code: this.request.verifyMethod
-      })
-
-    console.log("Verification resposne from twilio; got", twilioResponse)
-    return (twilioResponse.status === 'approved')
-  }
-}
 
 const confirm = async (req, res) => {
   const {verificationId, verificationCode} = identity.decrypt(req.body);
@@ -298,12 +84,14 @@ const confirm = async (req, res) => {
   const verification = verifications.get(verificationId);
   const contactPointVerified = await verification.processChallengeResponse()
 
-  let event = genReturn(true)
   if (!contactPointVerified){
-      event = genReturn(false, _errors.FAILED_PHONE_VERIFICATION, 'Failed phone verification')
+      return ['verifications/failed', {
+        id: v.id,
+        code: _errors.FAILED_PHONE_VERIFICATION,
+        detail: "Failed phone verification"
+      }]
   }
-
-  verification.processEvent(event)
+  return ['verifications/complete-verify-phone', { id: v.id, }]
 };
 
 
@@ -325,8 +113,8 @@ const verify = (req, res) => {
   // TODO: decrypt the request using the identity.
   const plain = identity.decrypt(req.body);
   console.log(JSON.stringify(plain, null, 2)); // XXX
-
-  const request = new VerificationRequest(plain);
+  const v = createVerification(plain)
+  store.dispatch('verifications/add', v)
 
   // TODO: should the response be encrypted using the UI's DID?
   res.send(request.id);
@@ -356,3 +144,258 @@ app.use(function (req, res, next) {
 });
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
+
+
+
+const createVerification = (request) => ({
+    creationTs: Date.now(),
+    id: uuid.v4(),
+    request: request,
+    challenge: null,
+    retrievedFhirResourceBody: null,
+    status: _verificationStates.INIT,
+    error: null
+})
+
+const store = createStore([store => {
+  store.on('@init', () => {
+    return {verifications: {}}
+  })
+
+  store.on('verifications/add', ({verifications}, v) => ({
+    verifications: {...verifications, ...{
+      [v.id]: v
+    }}
+  }))
+
+  store.on('verifications/establish', ({verifications}, { id }) => ({
+    verifications: {
+      ...verifications,
+      [id]: {
+        ...verifications[id],
+        status: _verificationStates.ESTABLISHED
+      }
+    }
+  }));
+
+  store.on('verifications/verify-fhir', ({verifications}, { id }) => ({
+    verifications: {
+      ...verifications,
+      [id]: {
+        ...verifications[id],
+        status: _verificationStates.FHIR_VERIFIED
+      }
+    }
+  }));
+
+  store.on('verifications/begin-verify-phone', ({verifications}, { id }) => ({
+    verifications: {
+      ...verifications,
+      [id]: {
+        ...verifications[id],
+        status: _verificationStates.CONTACT_VERIFYING
+      }
+    }
+  }));
+
+  store.on('verifications/complete-verify-phone', ({verifications}, { id }) => ({
+    verifications: {
+      ...verifications,
+      [id]: {
+        ...verifications[id],
+        status: _verificationStates.CONTACT_VERIFIED
+      }
+    }
+  }));
+
+
+  store.on('verifications/failed', ({verifications}, {id, error, detail}) => ({
+    verifications: {
+      ...verifications,
+      [id]: {
+        ...verifications[id],
+        status: _verificationStates.ERROR,
+        error,
+        detail
+      }
+    }
+  }));
+
+}])
+
+async function verificationFhirResource(v) {
+
+  let url = new URL(
+    `${v.request.resourceType}/${v.request.resourceId}`,
+    v.request.fhirBaseUrl
+  ).toString();
+
+  // **** build the headers for our FHIR request ****
+
+  let headers = new Headers();
+  headers.append('Accept', 'application/fhir+json');
+  headers.append('Authorization', v.request.authToken);
+
+  // **** issue our request ****
+
+  let response = await fetch(url, {
+    method: 'GET',
+    headers: headers,
+  });
+
+  // **** check for status ****
+
+  if (!response.ok) {
+      return ['verifications/failed', {
+        id: v.id,
+        code: _errors.FAILED_TO_CONNECT_TO_FHIR_SERVER,
+        detail: `Request to ${url} failed: ${response.status} - ${response.statusText}`
+      }]
+  }
+
+    // **** grab the body of the response ****
+
+    const resourceBody = await response.json();
+
+    if (!resourceBody) {
+      return ['verifications/failed', {
+        id: v.id,
+        code: _errors.RESOURCE_NOT_FOUND,
+        detail: `Request to ${url} returned no content.`
+      }]
+    }
+
+    return tryValidateResourceClaim(v, resourceBody)
+
+}
+
+  /**
+   * Check a retrieved resource against the verification info
+   */
+  function tryValidateResourceClaim(v, resourceBody) {
+    // **** act depending on resource type ****
+
+    switch (resourceBody.resourceType) {
+      case 'Patient':
+        if (fhirPatientHasContactPoint(
+          resourceBody,
+          v.request.contactPoint
+        )) {
+          return ['verifications/verify-fhir', {
+            id: v.id,
+            resourceBody
+          }];
+        }
+        return ['verifications/failed', {
+          id: v.id,
+          code:  _errors.CONTACT_POINT_NOT_FOUND,
+          detail: `Contact point not found! ${JSON.stringify(v.request.contactPoint)}`
+      }]
+   }
+
+    // **** unknown resource type, cannot verify ****
+        return ['verifications/failed', {
+          id: v.id,
+          code:  _errors.INVALID_ARGUMENT,
+          detail: `Unsupported resource type: ${v.request.resourceType}`
+      }]
+  }
+
+
+const verificationEstablish = async v => {
+  console.log("going to establish", v)
+    // **** sanity checks ****
+
+    if (!v.request) {
+    return ['verifications/failed', {
+      id: v.id,
+      code: _errors.INVALID_ARGUMENT,
+      detail: 'Request not found'
+    }]
+    }
+
+    if (!v.request.fhirBaseUrl) {
+    return ['verifications/failed', {
+      id: v.id,
+      code: _errors.INVALID_ARGUMENT,
+      detail: 'No FHIR Server URL'
+    }]
+    }
+
+    if (!v.request.resourceType) {
+    return ['verifications/failed', {
+      id: v.id,
+      code: _errors.INVALID_ARGUMENT,
+      detail: 'No FHIR Resource Type'
+    }]
+
+    }
+
+    if (!v.request.resourceId) {
+    return ['verifications/failed', {
+      id: v.id,
+      code: _errors.INVALID_ARGUMENT,
+      detail: 'No FHIR Resource ID'
+    }]
+    }
+
+    // **** success ****
+
+    return ['verifications/establish', {
+      id: v.id
+    }]
+
+}
+async function issueChallenge(v) {
+    // TODO: raise an exception if Twilio is down or the challenge fails to send
+    console.log("Twilio issue", v.request.contactPoint.value, v.request.verifyMethod)
+    try {
+    const twilioResponse = await twilioClient
+      .verify.services(twilioConfig.serviceId)
+      .verifications
+      .create({
+        to: v.request.contactPoint.value,
+        channel: v.request.verifyMethod
+      })
+      console.log("Twilio issued", twilioResponse)
+
+    } catch(err) {
+      console.log("Twilio err", err)
+      return ['verifications/failed', {
+        id: v.id,
+        code: _errors.FAILED_PHONE_VERIFICATION,
+        detail: `Could not create verification request for ${v.request.contactPoint.value}: ${err}`
+      }]
+
+
+    }
+    return ['verifications/begin-verify-phone', { id: v.id, }];
+ }
+
+store.on("@changed", (...args) =>  {
+  console.log("Changed", store.get())
+})
+
+store.on("@dispatch", async (state, [event, data]) => {
+  if (event === 'verifications/add') {
+    const nextResult = await verificationEstablish(data);
+    store.dispatch(...nextResult)
+  } else if (event === 'verifications/establish') {
+    const nextResult = await verificationFhirResource(state.verifications[data.id]);
+    store.dispatch(...nextResult)
+  } else if (event === 'verifications/verify-fhir') {
+    const nextResult = await issueChallenge(state.verifications[data.id]);
+    store.dispatch(...nextResult)
+  }
+
+})
+
+const v = createVerification({
+  fhirBaseUrl: "https://r4.smarthealthit.org",
+  resourceType: "Patient",
+  resourceId: "835a3b08-e3b6-49c4-a23f-1fac8dc7a7a7",
+  contactPoint: { system: 'phone', value: '555-258-5879', use: 'home' },
+  verifyMethod: 'sms'
+})
+
+store.dispatch('verifications/add', v)
